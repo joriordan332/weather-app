026e485de48186ce8499fd6fd25bac2d7dd49655
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-08-01 10:02:10 +0100

    Enter eventListener added & object code refactored
    An enter eventListener added so that user doesn't just have to click submit to search
    PopulateWeather function refactored so that dot notation can be used now to access specific object value

--- script.js
+++ script.js
@@ -3,32 +3,54 @@ const search = document.querySelector('.search');
 
 search.addEventListener('click', async () => {
 	try {
-const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=847c030025fe4efa829142248232707&q='${weatherInput.value}&days=1&aqi=no&alerts=no'`, {mode: 'cors'});
-const weatherData = await response.json();
-console.log(weatherData);
-/*img.src = weatherData.*****.*****.***;*/
-populateWeather(weatherData)
-} catch (error){
-	console.log('Please enter a valid City');
-}
+		const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=847c030025fe4efa829142248232707&q='${weatherInput.value}&days=1&aqi=no&alerts=no'`, {mode: 'cors'});
+		const weatherData = await response.json();
+		console.log(weatherData);
+		/*img.src = weatherData.*****.*****.***;*/
+		populateWeather(weatherData)
+		} catch (error){
+			console.log('Please enter a valid City');
+		}
 
 weatherInput.value = '';
 });
 
+weatherInput.addEventListener('keyup', async (e) => {
+	if(e.key === 'Enter') {
+		try {
+			const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=847c030025fe4efa829142248232707&q='${weatherInput.value}&days=1&aqi=no&alerts=no'`, {mode: 'cors'});
+			const weatherData = await response.json();
+			console.log(weatherData);
+			/*img.src = weatherData.*****.*****.***;*/
+			populateWeather(weatherData)
+			} catch (error){
+				console.log('Please enter a valid City');
+			}
+
+weatherInput.value = '';
+	}
+});
+
+
 
 
 function populateWeather(obj) {
-	const currentWeather = 
-	`Location: ${obj.location.name} 
-	Country: ${obj.location.country}
-	Time: ${obj.location.localtime}
-	Temp C: ${obj.current.temp_c} 
-	Temp F: ${obj.current.temp_f} 
-	Feels like C: ${obj.current.feelslike_c}
-	Feels like F: ${obj.current.feelslike_f}
-	Condition: ${obj.current.condition.text} ${obj.current.condition.icon}
-	Humidity: ${obj.current.humidity} 
-	Wind MPH: ${obj.current.wind_mph} 
-	Chance of rain: ${obj.forecast.forecastday[0].day.daily_chance_of_rain}%`;
-	console.log(currentWeather)
+	const currentWeather = {
+	location: `${obj.location.name}`, 
+	country: `${obj.location.country}`,
+	time: `${obj.location.localtime}`,
+	temp_C: `${obj.current.temp_c}`, 
+	temp_F: `${obj.current.temp_f}`,
+	feels_like_C: `${obj.current.feelslike_c}`,
+	feels_like_F: `${obj.current.feelslike_f}`,
+	condition: `${obj.current.condition.text} ${obj.current.condition.icon}`,
+	humidity: `${obj.current.humidity}`, 
+	wind_MPH: `${obj.current.wind_mph}`, 
+	chance_of_rain: `${obj.forecast.forecastday[0].day.daily_chance_of_rain}%`
+	}
+	console.log(currentWeather.location)
+
+	const temperature = document.querySelector('.temperature');
+	temperature.textContent = currentWeather.temp_C
+
 }
