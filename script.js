c9de81d9c605350f89592fb708fa164cc4dcbfde
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-08-01 10:31:16 +0100

    Onload event added
    Onload event which will display the weather for my location when the page loads so that the app isn't blank when user loads the page

--- script.js
+++ script.js
@@ -31,7 +31,19 @@ weatherInput.value = '';
 	}
 });
 
+window.onload = async () => {
+	try {
+		const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=847c030025fe4efa829142248232707&q='Acklam&days=1&aqi=no&alerts=no'`, {mode: 'cors'});
+		const weatherData = await response.json();
+		console.log(weatherData);
+		/*img.src = weatherData.*****.*****.***;*/
+		populateWeather(weatherData)
+		} catch (error){
+			console.log('Please enter a valid City');
+		}
 
+weatherInput.value = '';
+};
 
 
 function populateWeather(obj) {
@@ -39,16 +51,17 @@ function populateWeather(obj) {
 	location: `${obj.location.name}`, 
 	country: `${obj.location.country}`,
 	time: `${obj.location.localtime}`,
-	temp_C: `${obj.current.temp_c}`, 
-	temp_F: `${obj.current.temp_f}`,
+	temp_C: `${obj.current.temp_c}°C`, 
+	temp_F: `${obj.current.temp_f}°F`,
 	feels_like_C: `${obj.current.feelslike_c}`,
 	feels_like_F: `${obj.current.feelslike_f}`,
-	condition: `${obj.current.condition.text} ${obj.current.condition.icon}`,
+	condition: `${obj.current.condition.text}`,
+	condition_icon: `${obj.current.condition.icon}`,
 	humidity: `${obj.current.humidity}`, 
 	wind_MPH: `${obj.current.wind_mph}`, 
 	chance_of_rain: `${obj.forecast.forecastday[0].day.daily_chance_of_rain}%`
 	}
-	console.log(currentWeather.location)
+	
 
 	const temperature = document.querySelector('.temperature');
 	temperature.textContent = currentWeather.temp_C
